37d1ac2a936048bfc90380eda3fd8138198a1927
PauloSantaLuce <136512968+PauloSantaLuce@users.noreply.github.com>
2024-08-07 09:21:46 -0600

    Java.js

--- Java.js
+++ Java.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("myForm")
-
+//llamamos a que formato editaremos
 form.addEventListener("submit", function(event){
     event.preventDefault();
-
+//Nombramos los 3 elementos, el espacio del nombre, del correo y del boton.
     const name = document.getElementById("name").value;
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
